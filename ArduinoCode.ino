de0178ac948afea95d06e6da71d54d85eddb56d9
C E Z A#6466 <93098721+cezacs1@users.noreply.github.com>
2023-12-04 21:59:14 +0300

    Create ArduinoCode.ino

---
+++ ArduinoCode.ino
@@ -0,0 +1,34 @@
+#include <Servo.h>
+
+Servo servoMotor;
+
+void setup() {
+  servoMotor.attach(9);
+  Serial.begin(9600);
+  servoMotor.write(90);
+}
+
+char oldvalue = ' ';
+bool alreadyRotated = false;
+
+void loop() {
+  if (Serial.available() > 0) { 
+    char receivedChar = Serial.read();
+
+    if (oldvalue == receivedChar) {
+      return;
+    } else {
+      oldvalue = receivedChar;
+
+      if (receivedChar == '1' && !alreadyRotated) {
+        servoMotor.write(83);
+        delay(500);
+        servoMotor.write(99);
+        delay(500);
+
+        servoMotor.write(90);
+        //alreadyRotated = true;
+      }
+    }
+  }
+}
