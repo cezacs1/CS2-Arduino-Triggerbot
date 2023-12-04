9c84739c7d32b4bad0a2d9b40abb01528c8c1a93
C E Z A#6466 <93098721+cezacs1@users.noreply.github.com>
2023-12-04 21:58:01 +0300

    Update ArduinoCode.ino
    Daha hızlı tıklama.

--- ArduinoCode.ino
+++ ArduinoCode.ino
@@ -2,39 +2,32 @@
 
 Servo servoMotor;
 
-void setup()
-{
+void setup() {
   servoMotor.attach(9);
   Serial.begin(9600);
   servoMotor.write(90);
 }
 
-char oldvalue = ' ';
-bool alreadyRotated = false;
+char oldvalue = ' '; // Tek bir karakter olarak tanımlanmış ve boş bir karakterle başlatılmış
+bool alreadyRotated = false; // Servo motorun zaten döndürüldüğünü kontrol etmek için bir bayrak kullanılıyor
 
-void loop()
-{
-  if (Serial.available() > 0)
-  { 
+void loop() {
+  if (Serial.available() > 0) { 
     char receivedChar = Serial.read();
 
-    if (oldvalue == receivedChar)
-    {
+    if (oldvalue == receivedChar) {
       return;
-    }
-    else
-    {
+    } else {
       oldvalue = receivedChar;
 
-      if (receivedChar == '1' && !alreadyRotated)
-      {
+      if (receivedChar == '1' && !alreadyRotated) {
         servoMotor.write(83);
-        delay(1000);
+        delay(500);
         servoMotor.write(99);
-        delay(1000);
+        delay(500);
 
         servoMotor.write(90);
-        //alreadyRotated = true; // Servo motor döndürüldüğünde true yap
+        //alreadyRotated = true; // Servo motor döndürüldüğünde bayrağı true yap
       }
     }
   }
